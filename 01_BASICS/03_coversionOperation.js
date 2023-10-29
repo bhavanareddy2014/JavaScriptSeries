7f917862b7b94f7892809eec2abec753654007a9
bhavanareddy2014 <75924452+bhavanareddy2014@users.noreply.github.com>
2023-10-29 07:12:27 +0000

    coversion of datatypes

---
+++ 01_BASICS/03_coversionOperation.js
@@ -0,0 +1,29 @@
+let score = "33abc"
+
+console.log(typeof score);
+console.log(typeof(score))
+let value =  Number(score);
+console.log(typeof value);
+console.log(value);
+
+// "33" =>  33
+//"33abc" => NaN
+// true => 1; false => 0
+
+let loggedIn =  1
+
+let booleanLoggedIn = Boolean(loggedIn)
+
+console.log(booleanLoggedIn);
+
+// 1 =>  true;  0 => false
+
+//"" => false
+
+// "hitesh" => true
+
+let string =  33
+
+let stringNumber = String(string)
+
+console.log(typeof stringNumber);

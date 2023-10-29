406206f78102ee55dc585db912d30f49a50dbd56
bhavanareddy2014 <75924452+bhavanareddy2014@users.noreply.github.com>
2023-10-29 06:57:22 +0000

    datatypes

---
+++ 01_BASICS/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = "bhavana";
+let accountEmail = "reddy@gmail.com";
+var accountPassword ="121212";
+accountCity ="Jaipur";
+
+accountCity ="bengaluru";
+accountEmail="hello@gmail.com";
+accountPassword="212121";
+console.table([accountCity,accountEmail,accountPassword])
+console.log(accountPassword)
+
+/*
+prefer not to use var using due to issue in block and functionalScope
+*/
---
+++ 01_BASICS/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all js code as newer version
+
+//alert(3 + 3); //  we are using nodejs, not browser
+
+// code readability is high
+
+let name ="bhavana";
+let age = 25;
+
+//primitive Datatypes
+
+//number => 2 to power 53
+//bigInt
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => not value assigned
+//symbol => unique
+
+//object
+
+console.log(typeof "hites"); // string 
+console.log(typeof null); // object
+console.log(typeof undefined) // undefined
